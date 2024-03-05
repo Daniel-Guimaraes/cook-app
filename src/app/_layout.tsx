fc0e419cef7e92c57e1281e26612497aed4a5060
Daniel Vieira <daniel.guimaraes.vieira.dev@gmail.com>
2024-03-04 22:45:57 -0300

    feat: create layout default application

---
+++ src/app/_layout.tsx
@@ -0,0 +1,21 @@
+import {
+  Poppins_400Regular,
+  Poppins_500Medium,
+  Poppins_700Bold,
+  useFonts,
+} from '@expo-google-fonts/poppins'
+import { Slot } from 'expo-router' // É como se fosse o children do react
+
+export default function Layout() {
+  const [fontsLoaded] = useFonts({
+    Poppins_400Regular,
+    Poppins_500Medium,
+    Poppins_700Bold,
+  })
+
+  if (!fontsLoaded) {
+    return
+  }
+
+  return fontsLoaded ? <Slot /> : null
+}
